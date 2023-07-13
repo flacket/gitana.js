cfe1f7a8bd96760a7b2f6f560442ece8f8eb6318
flacket <facundoggallardo@gmail.com>
2023-07-12 23:43:03 -0300

    Firebase Auth Login and Logout configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import {getAuth} from "firebase/auth"
 
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    component: () => import('@/layouts/DefaultLayout.vue'),
     children: [
       {
         path: '',
@@ -12,7 +13,15 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: () => import('@/views/HomePage.vue'),
+      },
+      {
+        path: "acerca",
+        name: "acerca",
+        component: () => import('@/views/AcercaPage.vue'),
+        meta: {
+          requiresAuth: true,
+        }
       },
     ],
   },
@@ -22,5 +31,17 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 })
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (getAuth().currentUser) {
+      next();
+    } else {
+      alert("No tienes acceso");
+      next("/");
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
