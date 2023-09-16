5b65e881847c49949ae2249b3778b2b6cd3a9666
flacket <facundoggallardo@gmail.com>
2023-09-16 13:12:11 -0300

    remove Apollo code + dependencies

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
 // Components
 import App from './App.vue'
 
 // Composables
-import { createApp, h } from 'vue'
+import { createApp } from 'vue'
 
 // Plugins
 import { registerPlugins } from '@/plugins'
 
-//ApolloClient
-import apolloClient from '@/apollo'
-//import { DefaultApolloClient } from '@vue/apollo-composable'
-import { provideApolloClient } from "@vue/apollo-composable";
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -30,19 +20,14 @@ const firebaseConfig = {
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_FIREBASE_PROJECT_ID + ".appspot.com",
     messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
-    appId: "1:" + import.meta.env.VITE_FIREBASE_SENDER_ID + 
+    appId: "1:" + import.meta.env.VITE_FIREBASE_SENDER_ID +
     ":web:" + import.meta.env.VITE_FIREBASE_APP_ID
-};
+}
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+initializeApp(firebaseConfig)
 
-const app = createApp({
-    setup() {
-        provideApolloClient(apolloClient);
-    },
-    render: () => h(App)
-});
+const app = createApp(App)
 
 registerPlugins(app)
 
